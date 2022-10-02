e7b85ad339439b944ed6c2c2cdfcae99f578f22c
Golodhrim <holidayninjastaff@gmail.com>
2022-10-02 15:54:31 -0400

    railway expose issues

--- config/server.js
+++ config/server.js
@@ -1,6 +1,5 @@
-const PORT = process.env.PORT;
 module.exports = ({ env }) => ({
-  host: env("HOST", `0.0.0.0:${PORT}`),
+  host: env("HOST", `0.0.0.0`),
   port: env.int("PORT", 1337),
   admin: {
     auth: {
