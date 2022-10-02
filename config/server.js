51ab259782cce0952964d52cb10da572ecc3aaba
Golodhrim <holidayninjastaff@gmail.com>
2022-10-02 15:44:37 -0400

    railway server update

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", `0.0.0.0:${PORT}`),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '121e6f9f125705d1a072d4e69f683d72'),
+      secret: env("ADMIN_JWT_SECRET", "121e6f9f125705d1a072d4e69f683d72"),
     },
   },
 });
